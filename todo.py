17ead6b4d2d3b8eea25fb06d28abbc64ef3c2d44
topleft <pete.james.jeffryes@gmail.com>
2015-03-13 06:23:38 -0600

    Format date using template

--- todo.py
+++ todo.py
@@ -1,5 +1,5 @@
 import sqlite3
-from bottle import route, run, debug
+from bottle import route, run, debug, template
 
 @route('/todo')
 
@@ -8,6 +8,8 @@ def todo_list():
 	c = conn.cursor()
 	c.execute("SELECT id, task FROM todo WHERE status LIKE '1'")
 	result = c.fetchall()
-	return str(result)
+	c.close()
+	output = template('make_table', rows=result)
+	return output
 debug(True)
 run(reloader=True)
