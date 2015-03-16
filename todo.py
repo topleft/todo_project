44cf73423fb702352c8fb048c8eea38d1a301860
topleft <pete.james.jeffryes@gmail.com>
2015-03-16 07:17:14 -0600

    Added an edit page to edit tasks

--- todo.py
+++ todo.py
@@ -30,5 +30,31 @@ def new_item():
 		return template('new_task.tpl')
 
 
+@route('/edit/:no', method='GET')
+def edit_item(no):
+	if request.GET.get('save', '').strip():
+		edit = request.GET.get('task', '').strip()
+		status = request.GET.get('status', '').strip()
+
+		if status == 'open':
+			status = 1
+		else:
+			status = 0
+
+		conn = sqlite3.connect('todo.db')
+		c = conn.cursor()
+		c.execute('UPDATE todo SET task = ?, status = ? WHERE id LIKE ?', (edit, status, no))
+		conn.commit()
+
+		return '<p>The item number %s was successfully updated</>' % no
+
+	else:
+		conn = sqlite3.connect('todo.db')
+		c = conn.cursor()
+		c.execute('SELECT task FROM todo WHERE id LIKE ?', (str(no)))
+		cur_data = c.fetchone()
+
+		return template('edit_task', old=cur_data, no=no)
+
 debug(True)
 run(reloader=True)
